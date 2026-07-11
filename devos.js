f29e67a1fffedd0ccb79b647b4d19e618710a514
Gautam Rishi <gautamyadav73240@gmail.com>
2026-07-11 14:08:03 +0530

    Add files via upload

---
+++ devos.js
@@ -0,0 +1,28 @@
+// const btn = document.getElementById("learnBtn");
+
+// btn.addEventListener("click", () => {
+
+//     document.getElementById("about").scrollIntoView({
+//         behavior: "smooth"
+//     });
+
+// });
+
+// const cards = document.querySelectorAll(".card,.member");
+
+// window.addEventListener("scroll",()=>{
+
+// cards.forEach(card=>{
+
+// const top = card.getBoundingClientRect().top;
+
+// if(top<window.innerHeight-100){
+
+// card.style.opacity="1";
+// card.style.transform="translateY(0)";
+
+// }
+
+// });
+
+// });
